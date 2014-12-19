29b9eed6a59476d260dd56f8c9737fc4979f5c5c
Mel <mel@scmb-sbeats03m.staff.science.uq.edu.au>
2014-12-19 14:33:54 +1000

    Reverse complement fasta sequence in Python

---
+++ reverse_complement_fasta.py
@@ -0,0 +1,17 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_nucleotide
+from Bio.Alphabet import IUPAC
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+import sys
+ 
+with open(sys.argv[1], 'r') as fin: # Open the infile (fasta)
+    with open(sys.argv[2], 'w') as fout: # Create and open the outfile (fasta)
+	# To open a file format other than fasta, change "fasta"
+	# I.e. "gb" or "embl"
+	# If your file is multifasta, this will reverse complement all sequences 
+        for seq_record in SeqIO.parse(fin, "fasta"): 
+            forward_seq = seq_record.seq 			# obtain sequence
+            reverse_seq = forward_seq.reverse_complement()	# reverse complement
+            reverse_rec = SeqRecord(reverse_seq, seq_record.id)	# create new record
+            SeqIO.write(reverse_rec, fout, "fasta")		# write out reverse complemented sequence
